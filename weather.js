aa58b3c980daf3a354612073516b09731ac60edf
Chelsea Neider <chelseaneider@gmail.com>
2020-04-22 02:42:52 -0600

    intial commit

--- weather.js
+++ weather.js
@@ -1,26 +1,34 @@
 $(document).ready(function(){
     $("#submitWeather").click(function(){
-       var city = $("city").val();
-       if(city != ''){
-   
-           $.ajax({
+        console.log("click");
+         var city = $("#textcity").val();
+         console.log("textcity: ", city)
+         let url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + "&untis=metric" + "&appid=ccbfb8daf2a8d8b84a4a6870052f1a52";
+           
+         $.ajax({
              
-               url:'https://api.openweatermap.org/data/2.5/weather?q=' + city + "&untis=metric" +
-               "&APPID=ccbfb8daf2a8d8b84a4a6870052f1a52";
-               type:"GET",
-               dataType:"jsonp"
+               url: url,
                success: function(data){
+                var fa = function(k){
+                   let c = k - 273;
+                   let f = 1.8 * c + 32;
+                   return f 
+                }
+               $("#city")
+                 .html(`<div>${data.name}</div>`)
+               $("#date")
+                 .html(`<div>${Date(data.dt)}</div>`)  
+               $("#temperature")
+                 .html(`<div>${fa(data.main.temp)}</div>`)  
+               $("#skies")   
+                 .html(`<div>${data.weather[0].description}</div>`)
+               $("#hi-lo")
+                  .html(`<div>${fa(data.main.temp_min)}f/${fa(data.main.temp_max)}f</div>`)               
                   console.log(data);
 
                }
    
            });
    
-       }else {
-           $("#error").html('Field cannot be empty');
-       }
    });
 });
-
-
-   
